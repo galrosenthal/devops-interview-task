29a59322e67dae4c800f011d5b8d816d8d20768f
Gal <galrosenthal@gmail.com>
2025-07-13 19:39:54 +0000

    Docker file and edit ip listen on main.py

--- app/main.py
+++ app/main.py
@@ -8,4 +8,4 @@ def root():
     return jsonify({"status": "ok", "value": random.randint(1, 100)})
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000)
+    app.run(host="0.0.0.0", port=5000)
